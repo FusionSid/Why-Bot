88cc1e704e6809bc0709f717c55d28a190ba25a1
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2021-10-11 22:59:00 +0000

    e

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -6,41 +6,42 @@ class Fun(commands.Cog):
   def __init__(self, client):
     self.client = client
 
-    @commands.command()
-    async def rps(self, ctx, rps:str):
-        choices = ["Rock", "Paper", "Scissors"]
-        cpu_choice = random.choice(choices)
-        em = discord.Embed(title="Rock Paper Scissors")
-        if rps == 'rock':
-            if cpu_choice == 'rock':
-                em.title = "It's a tie!"
-            elif cpu_choice == 'scissors':
-                em.title = "You win!"
-            elif cpu_choice == 'paper':
-                em.title = "You lose!"
+  @commands.command()
+  async def rps(self, ctx, rps:str):
+      choices = ["rock", "paper", "scissors"]
+      cpu_choice = random.choice(choices)
+      em = discord.Embed(title="Rock Paper Scissors")
+      rps = rps.lower()
+      if rps == 'rock':
+          if cpu_choice == 'rock':
+              em.description = "It's a tie!"
+          elif cpu_choice == 'scissors':
+              em.description = "You win!"
+          elif cpu_choice == 'paper':
+              em.description = "You lose!"
 
-        elif rps == 'paper':
-            if cpu_choice == 'paper':
-                em.title = "It's a tie!"
-            elif cpu_choice == 'rock':
-                em.title = "You win!"
-            elif cpu_choice == 'scissors':
-                em.title = "You lose!"
+      elif rps == 'paper':
+          if cpu_choice == 'paper':
+              em.description = "It's a tie!"
+          elif cpu_choice == 'rock':
+              em.description = "You win!"
+          elif cpu_choice == 'scissors':
+              em.description = "You lose!"
 
-        elif rps == 'scissors':
-            if cpu_choice == 'scissors':
-                em.title = "It's a tie!"
-            elif cpu_choice == 'paper':
-                em.title = "You win!"
-            elif cpu_choice == 'rock':
-                em.title = "You lose!"
+      elif rps == 'scissors':
+          if cpu_choice == 'scissors':
+              em.description = "It's a tie!"
+          elif cpu_choice == 'paper':
+              em.description = "You win!"
+          elif cpu_choice == 'rock':
+              em.description = "You lose!"
 
-        else:
-            em.title = "Invalid Input"
+      else:
+          em.description = "Invalid Input"
 
-        em.add_field(name="Your Choice", value=rps)
-        em.add_field(name="Bot Choice", value=cpu_choice)
-        await ctx.send(embed=em)
+      em.add_field(name="Your Choice", value=rps)
+      em.add_field(name="Bot Choice", value=cpu_choice)
+      await ctx.send(embed=em)
 
 
 def setup(client):
