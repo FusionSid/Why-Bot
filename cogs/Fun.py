741ecc1a4e8062974e1e402f339c4e22b9df68dd
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2021-10-11 23:26:40 +0000

    del number guesser - useless

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -43,37 +43,7 @@ class Fun(commands.Cog):
         em.add_field(name="Bot Choice", value=cpu_choice)
         await ctx.send(embed=em)
 
-    @commands.command()
-    async def numberguess(self, ctx):
-        num = random.randint(0, 100)
-        tries = 0
-        errorem = discord.Embed(title = "Invalid Input", description = "Please enter a valid number from 0 - 100")
-        em = discord.Embed(title="Number Guesser")
-
-        while guess != num:
-            guess = await self.client.wait_for("message", check=wfcheck)
-            try:
-                guess = int(guess)
-                tries += 1
-
-                if guess == num:
-                    em.description = f"Correct\nYou guessed it in {tries} tries"
-                elif guess > num:
-                    em.description("To high guess lower")
-                elif guess < num:
-                    em.description("To low guess higher")
-
-                elif guess > 100:
-                    await ctx.send(embed=errorem)
-                elif guess < 0:
-                    await ctx.send(embed=errorem)
-                else:
-                    await ctx.send(embed=errorem)
-
-                await ctx.send(embed = em)
-            except:
-                await ctx.send(embed = errorem)
-            
+    
 
 def setup(client):
     client.add_cog(Fun(client))
