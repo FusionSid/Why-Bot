9a90036c0d6d604d5ebafdddd97793ade589f36c
Siddhesh Zantye <siddheshadsv@icloud.com>
2021-10-12 13:00:29 +1300

    Added a bunch of cogs

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from roastlist import roastlistpy
 
 class Fun(commands.Cog):
     def __init__(self, client):
@@ -43,7 +44,29 @@ class Fun(commands.Cog):
         em.add_field(name="Bot Choice", value=cpu_choice)
         await ctx.send(embed=em)
 
-    
+
+    @commands.command()
+    async def roast(ctx):
+        roast = random.choice(roastlistpy)
+        em = discord.Embed(title=roast)
+        await ctx.send(embed=em)
+
+
+    @commands.command()
+    async def dm(ctx, member: discord.Member, *, message):
+        await ctx.channel.purge(limit=1)
+        embeddm = discord.Embed(title=message)
+        await member.send(embed=embeddm)
+
+
+    @commands.command()
+    async def sendroast(ctx, member: discord.Member):
+        await ctx.channel.purge(limit=1)
+        message = random.choice(roastlistpy)
+        embeddm = discord.Embed(
+            title=message, description="Imagine being roasted by a bot")
+        await member.send(embed=embeddm)
+
 
 def setup(client):
     client.add_cog(Fun(client))
