8f080c83ca4ea723d47b849982817302a87adaf6
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2021-10-21 05:00:22 +0000

    Working Poll system

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -4,6 +4,7 @@ import os
 import random
 from discord.ext import commands
 import asyncio
+from discord.utils import get
 
 
 roastlistpy = [
@@ -251,7 +252,7 @@ class Fun(commands.Cog):
 
 
     @commands.command(pass_context=True)
-    async def quickpoll(self, ctx, question, *options: str):
+    async def poll(self, ctx, time:int, question, *options: str):
         if len(options) <= 1:
             await ctx.send('You need more than one option to make a poll!')
             return
@@ -265,44 +266,27 @@ class Fun(commands.Cog):
             reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
 
         description = []
+        reacting = []
         for x, option in enumerate(options):
-            description += '\n {} {}'.format(reactions[x], option)
+            description += '\n{} = {}'.format(reactions[x], option)
         embed = discord.Embed(title=question, description=''.join(description))
         react_message = await ctx.send(embed=embed)
         for reaction in reactions[:len(options)]:
-            await self.bot.add_reaction(react_message, reaction)
-        embed.set_footer(text='Poll ID: {}'.format(react_message.id))
-        await self.bot.edit_message(react_message, embed=embed)
+            await react_message.add_reaction(reaction)
+            reacting.append(reaction)
+        await asyncio.sleep(time)
+        message = await ctx.channel.fetch_message(react_message.id)
+        results = {}
+        for i in reacting:
+          reaction = get(message.reactions, emoji=i)
+          count = reaction.count-1
+          results[i] = count
+        results = f'{results}'
+        results = results.replace("{", "")
+        results = results.replace("}", "")
+        results = results.replace("'", "")
+        await ctx.send(f"Results: \n{results}")
 
-
-    @commands.command(pass_context=True)
-    async def tally(self, ctx, id):
-        poll_message = await self.bot.get_message(ctx.message.channel, id)
-        if not poll_message.embeds:
-            return
-        embed = poll_message.embeds[0]
-        if poll_message.author != ctx.message.server.me:
-            return
-        if not embed['footer']['text'].startswith('Poll ID:'):
-            return
-        unformatted_options = [x.strip() for x in embed['description'].split('\n')]
-        opt_dict = {x[:2]: x[3:] for x in unformatted_options} if unformatted_options[0][0] == '1' \
-            else {x[:1]: x[2:] for x in unformatted_options}
-        # check if we're using numbers for the poll, or x/checkmark, parse accordingly
-        voters = [ctx.message.server.me.id]  # add the bot's ID to the list of voters to exclude it's votes
-
-        tally = {x: 0 for x in opt_dict.keys()}
-        for reaction in poll_message.reactions:
-            if reaction.emoji in opt_dict.keys():
-                reactors = await self.bot.get_reaction_users(reaction)
-                for reactor in reactors:
-                    if reactor.id not in voters:
-                        tally[reaction.emoji] += 1
-                        voters.append(reactor.id)
-
-        output = 'Results of the poll for "{}":\n'.format(embed['title']) + \
-                 '\n'.join(['{}: {}'.format(opt_dict[key], tally[key]) for key in tally.keys()])
-        await self.bot.say(output)
     
 def setup(client):
     client.add_cog(Fun(client))
