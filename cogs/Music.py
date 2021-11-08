ed465e4469354ec18034370b113afff1f1de894f
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2021-11-08 16:40:33 +1300

    e

--- cogs/Music.py
+++ cogs/Music.py
@@ -703,6 +703,23 @@ class Music(commands.Cog):
                     await voice.disconnect()
                 if not voice.is_connected():
                     break
+    
+    
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
 
+        if not member.id == self.client.user.id:
+            return
+
+        elif before.channel is None:
+            voice = after.channel.guild.voice_client
+            while voice.is_playing(): #Checks if voice is playing
+                await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+            else:
+                await asyncio.sleep(15) #If it's not playing it waits 15 seconds
+                while voice.is_playing(): #and checks once again if the bot is not playing
+                    break #if it's playing it breaks
+                else:
+                    await voice.disconnect() #if not it disconnects
 def setup(client):
     client.add_cog(Music(client))
