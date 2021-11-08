564a2f8b6cc8a9202a62cb0b097d2e5083e244ee
Siddhesh Zantye <28945611+FusionSid@users.noreply.github.com>
2021-11-08 03:42:04 +0000

    e

--- cogs/Music.py
+++ cogs/Music.py
@@ -684,26 +684,6 @@ class Music(commands.Cog):
         await asyncio.sleep(wait)
         os.remove(f'{name}.mp3')
         os.chdir(cd)
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-
-        if not member.id == self.client.user.id:
-            return
-
-        elif before.channel is None:
-            voice = after.channel.guild.voice_client
-            time = 0
-            while True:
-                await asyncio.sleep(1)
-                time = time + 1
-                if voice.is_playing() and not voice.is_paused():
-                    time = 0
-                if time == 10:
-                    await voice.disconnect()
-                if not voice.is_connected():
-                    break
-    
     
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -716,7 +696,7 @@ class Music(commands.Cog):
             while voice.is_playing(): #Checks if voice is playing
                 await asyncio.sleep(1) #While it's playing it sleeps for 1 second
             else:
-                await asyncio.sleep(15) #If it's not playing it waits 15 seconds
+                await asyncio.sleep(300) #If it's not playing it waits 15 seconds
                 while voice.is_playing(): #and checks once again if the bot is not playing
                     break #if it's playing it breaks
                 else:
