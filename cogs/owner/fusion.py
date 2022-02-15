6330157b37ab9b8509cc55f8122e740e9b5b4070
Siddhesh Zantye <siddheshadsv@icloud.com>
2022-02-16 00:31:10 +1300

    embeds for message_server

--- cogs/owner/fusion.py
+++ cogs/owner/fusion.py
@@ -40,18 +40,115 @@ class Fusion(commands.Cog):
     
     @commands.command()
     @commands.check(is_it_me)
-    async def message_servers(self, ctx, *, message):
+    async def message_servers(self, ctx, *, kwargs):
+        colors = {
+            "none" : None,
+            "blue": discord.Color.blue(),
+            "blurple": discord.Color.blurple(),
+            "brand_green": discord.Color.brand_green(),
+            "brand_red": discord.Color.brand_red(),
+            "dark_blue": discord.Color.dark_blue(),
+            "dark_gold": discord.Color.dark_gold(),
+            "dark_gray": discord.Color.dark_gray(),
+            "dark_green": discord.Color.dark_green(),
+            "dark_grey": discord.Color.dark_grey(),
+            "dark_magenta": discord.Color.dark_magenta(),
+            "dark_orange": discord.Color.dark_orange(),
+            "dark_purple": discord.Color.dark_purple(),
+            "dark_red": discord.Color.dark_red(),
+            "dark_teal": discord.Color.dark_teal(),
+            "dark_theme": discord.Color.dark_theme(),
+            "darker_gray": discord.Color.darker_gray(),
+            "darker_grey": discord.Color.darker_grey(),
+            "fuchsia": discord.Color.fuchsia(),
+            "gold": discord.Color.gold(),
+            "green": discord.Color.green(),
+            "greyple": discord.Color.greyple(),
+            "light_gray": discord.Color.light_gray(),
+            "light_grey": discord.Color.light_grey(),
+            "lighter_gray": discord.Color.lighter_gray(),
+            "lighter_grey": discord.Color.lighter_grey(),
+            "magenta": discord.Color.magenta(),
+            "nitro_pink": discord.Color.nitro_pink(),
+            "og_blurple": discord.Color.og_blurple(),
+            "orange": discord.Color.orange(),
+            "purple": discord.Color.purple(),
+            "random": discord.Color.random(),
+            "red": discord.Color.red(),
+            "teal": discord.Color.teal(),
+        }
+
+        colorlist = []
+        for c in colors:
+            colorlist.append(c)
+            
+        def wait_for_check(m):
+            return m.author == ctx.author and m.channel == ctx.message.channel
+
+        em = discord.Embed()
+        em.timestamp = datetime.datetime.utcnow()
+
+        kwargs = shlex.split(kwargs)
+        args = {}
+
+        for index in range(len(kwargs)):
+            if index % 2 == 0:
+                args[kwargs[index].lstrip("--")] = kwargs[index+1]
+            index += 0
+
+        for key, value in args.items():
+            print(len(args))
+            if key.lower() == "title":
+                em.title = value
+            elif key.lower() == "description" or key.lower() == "desc":
+                em.description = value
+            elif key.lower() == "img" or key.lower() == "image":
+                em.set_image(url=value)
+            elif key.lower() == "color" or key.lower() == "colour":
+                if value.lower() == "list" or value.lower() == "help":
+                    return await ctx.send(", ".join(colorlist))
+                if value.lower() not in colorlist:
+                    await ctx.send("Color not found", delete_after=2)
+                    em.color = ctx.author.color
+                else:
+                    em.color = colors[value.lower()]
+            elif key.lower() == "fields":
+                vint = False
+                try:
+                    int(value) 
+                    vint= True
+                except:
+                    vint = False
+                
+                if vint is True:
+                    for i in range(int(value)):
+                        entername = await ctx.send("Enter Name:")
+                        name = await self.client.wait_for("message", check=wait_for_check, timeout=300)
+                        await name.delete()
+
+                        entervalue = await ctx.send("Enter Value:")
+                        value = await self.client.wait_for("message", check=wait_for_check, timeout=300)
+                        await entername.delete()
+                        await entervalue.delete()
+                        await value.delete()
+
+                        em.add_field(name=name.content, value=value.content)
+            elif key.lower() in ["timestamp", "time"] and value.lower() in ["true", "yes"]:
+                em.timestamp = datetime.datetime.now()
+            else:
+                pass
+
         data = await self.client.get_db()
         for guild in self.client.guilds:
             if data[str(guild.id)]["announcement_channel"] is None:
                 try:
-                    await guild.system_channel.send(message)
+                    await guild.system_channel.send(embed=em)
                 except Exception:
                     continue
             else:
                 try:
                     channel = await self.client.fetch_channel(int(data[str(guild.id)]["announcement_channel"]))
-                    await channel.send(message)
+                    await channel.send(embed=em)
                 except Exception:
                     continue
     
