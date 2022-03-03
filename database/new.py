5519e6e480fe1a16aecc9774a2a4b748ae55815c
FusionSid <siddheshadsv@icloud.com>
2022-03-04 08:42:54 +1300

    Fixed autorole db issue

--- database/new.py
+++ database/new.py
@@ -4,7 +4,10 @@ with open("db.json") as f:
     data = json.load(f)
 
 for k, v in data.items():
-    v['settings']['plugins']['Logging'] = False
+    if  v['autorole']["all"] is None:
+        v['autorole']["all"] = []
+    if  v['autorole']["bot"] is None:
+        v['autorole']["bot"] = []
 
 with open('db.json', 'w') as f:
     json.dump(data, f, indent=4)
