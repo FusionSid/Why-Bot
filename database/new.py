a84962d3e017bb6d6b05933712ac564a01c75884
FusionSid <siddheshadsv@icloud.com>
2022-03-10 14:06:29 +1300

    Reset new.py

--- database/new.py
+++ database/new.py
@@ -3,11 +3,5 @@ import json
 with open("db.json") as f:
     data = json.load(f)
 
-for k, v in data.items():
-    if  v['autorole']["all"] is None:
-        v['autorole']["all"] = []
-    if  v['autorole']["bot"] is None:
-        v['autorole']["bot"] = []
-
 with open('db.json', 'w') as f:
     json.dump(data, f, indent=4)
