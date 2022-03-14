ea5f68082406168b0d85a34744117d24c10da4d6
FusionSid <siddheshadsv@icloud.com>
2022-03-15 10:13:47 +1300

    changed prefix to slash

--- cogs/setup/settings.py
+++ cogs/setup/settings.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import slash_command
 
 from main import get_prefix
 from log import log_errors
@@ -8,14 +9,14 @@ class Settings(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @slash_command(name="prefix", description="Shows the bots prefix")
     async def prefix(self, ctx):
         prefix = await get_prefix(self.client, ctx.message)
         em = discord.Embed(
             title=f"Hi, my prefix is `{prefix}`", 
             color=ctx.author.color
         )
-        return await ctx.send(embed=em)
+        return await ctx.respond(embed=em)
 
 def setup(client):
     client.add_cog(Settings(client))
