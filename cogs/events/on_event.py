3236e7c20699b1a4dd73ef5ab0eebdb91948e5c3
FusionSid <siddheshadsv@icloud.com>
2022-03-14 17:43:36 +1300

    created event files

---
+++ cogs/events/on_event.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from main import get_prefix
+
+class OnEvent(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+
+        if message.author.bot:
+            return
+
+        if self.client.user.mentioned_in(message) and message.mention_everyone == False and message.reference is None:
+            prefix = await get_prefix(self.client, message)
+            em = discord.Embed(
+                title=f"Hi, my prefix is `{prefix}`", 
+                color=message.author.color
+            )
+            return await message.channel.send(embed=em)
+
+def setup(client):
+    client.add_cog(OnEvent(client))
