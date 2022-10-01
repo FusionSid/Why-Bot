79ba03eb867f41db7130dbf82f9e1b09a06903e1
FusionSid <siddheshadsv@icloud.com>
2022-10-02 01:54:22 +1300

    on guild

---
+++ src/cogs/events/on_guild.py
@@ -0,0 +1,62 @@
+import datetime
+
+import discord
+from discord.ext import commands
+
+from core.models.client import WhyBot
+from core.utils.client_functions import update_activity
+
+
+class OnGuild(commands.Cog):
+    def __init__(self, client: WhyBot):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        """
+        Called when the bot is removed from a guild
+        It will update the bots activity
+        It will also send a message to the the leave_alert_channel which is set in the config
+        """
+        await update_activity(self.client)
+        leave_alert_channel = self.client.config["leave_alert_channel"]
+        if leave_alert_channel == 0 or leave_alert_channel == None:
+            return
+        try:
+            channel = self.client.get_channel(leave_alert_channel)
+        except discord.errors.NotFound:
+            return
+
+        em = discord.Embed(
+            title="Leave", description=f"Left: {guild.name}", color=discord.Color.red()
+        )
+        em.timestamp = datetime.datetime.utcnow()
+        await channel.send(embed=em)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        """
+        Called when the bot joins a guild
+        It will update the bots activity
+        It will also send a message to the the join_alert_channel which is set in the config
+        """
+        join_alert_channel = self.client.config["join_alert_channel"]
+        if join_alert_channel == 0 or join_alert_channel == None:
+            return
+        try:
+            channel = self.client.get_channel(join_alert_channel)
+        except discord.errors.NotFound:
+            return
+
+        em = discord.Embed(
+            title="Join",
+            description=f"Joined: {guild.name}",
+            color=discord.Color.green(),
+        )
+        em.timestamp = datetime.datetime.utcnow()
+        await channel.send(embed=em)
+        await update_activity(self.client)
+
+
+def setup(client: WhyBot):
+    client.add_cog(OnGuild(client))
