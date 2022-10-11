1c232b453b250f391057de61cf026cb89f3e4185
FusionSid <siddheshadsv@icloud.com>
2022-10-11 22:40:31 +1300

    safe calc + fast calc

--- src/core/utils/calc.py
+++ src/core/utils/calc.py
@@ -5,9 +5,13 @@ Used to calculate an expression using the mathjs api
 import urllib
 
 import aiohttp
+import numexpr
 
 
-async def calculate(expr: str, only_int: bool = False) -> str | int | None:
+async def slow_safe_calculate(expr: str, only_int: bool = False) -> str | int | None:
+    if expr.isnumeric():
+        return int(expr)
+
     expression = urllib.parse.quote(expr.replace("**", "^"))
     async with aiohttp.ClientSession() as session:
         async with session.get(f"http://api.mathjs.org/v4/?expr={expression}") as r:
@@ -18,5 +22,22 @@ async def calculate(expr: str, only_int: bool = False) -> str | int | None:
 
     if only_int:
         return int(result)
+
+    return result
+
+
+async def calculate(expr: str) -> int | None:
+
+    try:
+        result = numexpr.evaluate(expr)
+    except (
+        OverflowError,
+        AttributeError,
+        SyntaxError,
+        ZeroDivisionError,
+        KeyError,
+        ValueError,
+    ):
+        return None
 
     return result
