bad8f37d9986cf8171737ca27ba8f2b38ba2382f
FusionSid <siddheshadsv@icloud.com>
2022-10-26 23:06:46 +1300

    made it have placeholder fields to replace later

--- src/scripts/backup.sh
+++ src/scripts/backup.sh
@@ -2,4 +2,4 @@ path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PGPASSWORD="Replace With PSQL Password"
 name=`date +%d_%m_%Y`
 mkdir -p $path/backups
-pg_dump -Fc -h 168.138.102.186 -U sid whybot -f $path/backups/$name.dump
+pg_dump -Fc -h <psql server ip> -U <username> <database_name> -f $path/backups/$name.dump
