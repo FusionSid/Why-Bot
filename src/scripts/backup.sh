6bdd42b2340661aa558da0e72557601fedec15cc
FusionSid <siddheshadsv@icloud.com>
2022-10-26 22:05:48 +1300

    updated backup file names to d/m/y THE BETTER WAY

--- src/scripts/backup.sh
+++ src/scripts/backup.sh
@@ -1,5 +1,5 @@
 path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export PGPASSWORD="Replace With PSQL Password"
-name=`date +%m_%d_%Y`
+name=`date +%d_%m_%Y`
 mkdir -p $path/backups
 pg_dump -Fc -h 168.138.102.186 -U sid whybot -f $path/backups/$name.dump
