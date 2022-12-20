a6b714aac892eca4b9b190fe8d90e2ac13147895
FusionSid <siddheshadsv@icloud.com>
2022-12-20 15:01:48 +1300

    feat: Ticket system v1. Commands: /ticket new, /ticket delete_all Added button views for ticket closing and reopening. Tickets can have transcript viewed, be closed, be reopened and be deleted

--- src/cogs/moderation/tickets.py
+++ src/cogs/moderation/tickets.py
@@ -1,11 +1,15 @@
+import time
+
 import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup, default_permissions
 
 from core.models import WhyBot
-from core.models.ticket import TicketGuild, Ticket
+from core.helpers.views import ConfirmView
 from core.db.setup_guild import setup_tickets
+from core.utils.formatters import discord_timestamp
 from core.helpers.checks import run_bot_checks
+from core.models.ticket import TicketGuild, Ticket, TicketView
 
 
 class Tickets(commands.Cog):
@@ -27,11 +31,158 @@ class Tickets(commands.Cog):
 
     async def __get_tickets(self, guild_id: int):
         data: list[list] = await self.client.db.fetch(
-            "SELECT * FROM ticket_guild WHERE guild_id=$1", guild_id
+            "SELECT * FROM tickets WHERE guild_id=$1", guild_id
         )
         if len(data) == 0:
             return None
         return list(map(lambda x: Ticket(*x), data))
+
+    @ticket.command()
+    async def new(self, ctx: discord.ApplicationContext, reason=None):
+        await ctx.defer()
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if ctx.author.id in ticket_config.banned_users:
+            return await ctx.respond(
+                "You are banned from making tickets", ephemeral=True
+            )
+
+        # figure out if to make it in a category or not
+        if ticket_config.category in (0, None):
+            category = None
+        else:
+            category = list(
+                filter(lambda i: i.id == ticket_config.category, ctx.guild.categories)
+            )
+            category = None if len(category) == 0 else category[0]
+
+        # create channel
+        channel = await ctx.guild.create_text_channel(
+            f"ticket-{ctx.author.id}", category=category
+        )
+
+        # set channel perms
+        await channel.set_permissions(
+            ctx.guild.get_role(ctx.guild.id), send_messages=False, read_messages=False
+        )
+        perms = {
+            "send_messages": True,
+            "read_messages": True,
+            "add_reactions": True,
+            "embed_links": True,
+            "attach_files": True,
+            "read_message_history": True,
+            "external_emojis": True,
+        }
+        await channel.set_permissions(ctx.author, **perms)
+        for role in ticket_config.roles_allowed:
+            if role := ctx.guild.get_role(role) is not None:
+                await channel.set_permissions(role, **perms)
+
+        ticket_data = [ctx.guild.id, channel.id, ctx.author.id, int(time.time())]
+        ticket_id = await self.client.db.fetch(
+            """INSERT INTO tickets (
+                guild_id, channel_id, ticket_creator, time_created
+            ) VALUES ($1, $2, $3, $4) RETURNING id""",
+            *ticket_data,
+        )
+
+        ticket = Ticket(ticket_id[0][0], *ticket_data)
+
+        embed = discord.Embed(
+            title=f"New ticket from {ctx.author.name}!",
+            description=f"**Please wait, support will be with you shortly!**\
+                \n\nTicket Created: {await discord_timestamp(ticket.time_created, 'ts')}",
+        )
+        embed.add_field(name="Reason Provided:", value=str(reason))
+        embed.set_footer(text="To close this ticket click the close button")
+        view = TicketView(ticket, ctx.author, self.client)
+        await channel.send(embed=embed, view=view)
+
+        await ctx.respond(f"A new ticket has been created for you: {channel.mention}")
+
+    # TODO
+    # @ticket.command()
+    # async def close(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def rename(self, ctx: discord.ApplicationContext):
+    #     tickets = await self.__get_tickets(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def add_ping_role(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def remove_ping_role(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def add_allowed_role(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def remove_allowed_role(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def transcript(self, ctx: discord.ApplicationContext):
+    #     tickets = await self.__get_tickets(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def button(self, ctx: discord.ApplicationContext):
+    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+
+    # @ticket.command()
+    # @default_permissions(administrator=True)
+    # @commands.has_permissions(administrator=True)
+    # async def delete(self, ctx: discord.ApplicationContext):
+    #     tickets = await self.__get_tickets(ctx.guild.id)
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def delete_all(self, ctx: discord.ApplicationContext):
+        # send a button to confirm to close or not
+        are_you_sure = ConfirmView(target=ctx.author)
+        em = discord.Embed(
+            title="Delete ALL tickets?",
+            description=(
+                f"{ctx.author.mention} are you sure you want to close and delete ALL tickets?\n\
+                    This action can not be reversed"
+            ),
+            color=discord.Color.random(),
+        )
+        await ctx.respond(embed=em, view=are_you_sure, ephemeral=True)
+        await are_you_sure.wait()
+        if not are_you_sure.accepted:
+            return
+
+        tickets = await self.__get_tickets(ctx.guild.id)
+        channels = map(self.client.get_channel, (i.channel_id for i in tickets))
+        for channel in channels:
+            try:
+                await channel.delete()
+            except (AttributeError, discord.Forbidden):
+                continue
+        await self.client.db.execute(
+            "DELETE FROM tickets WHERE guild_id=$1", ctx.guild.id
+        )
+        await ctx.send("All tickets have been deleted!")
 
 
 def setup(client):
--- src/core/models/ticket.py
+++ src/core/models/ticket.py
@@ -1,5 +1,7 @@
+import io
+import chat_exporter
 from dataclasses import dataclass
-
+from core.helpers.views import ConfirmView
 import discord
 
 
@@ -26,7 +28,148 @@ async def get_button(guild_id: int) -> discord.ui.Button:
     return discord.ui.Button()
 
 
-class TicketView(discord.ui.View):
+class NewTicketView(discord.ui.View):
     def __init__(self, button: discord.ui.Button):
         super().__init__(timeout=None)
         self.add_item(button)
+
+
+class TicketView(discord.ui.View):
+    def __init__(self, ticket_state: Ticket, member: discord.Member, client):
+        self.member = member
+        self.ticket = ticket_state
+        self.client = client
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Close", style=discord.ButtonStyle.gray, emoji="🔒")
+    async def callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        # send a button to confirm to close or not
+        are_you_sure = ConfirmView(target=interaction.user)
+        em = discord.Embed(
+            title="Close Ticket?",
+            description=(
+                f"{interaction.user.mention} are you sure you want to close this ticket?\nThis action removes \
+                    access for the person who made the ticket from the channel\nHowever this action can be reversed"
+            ),
+            color=discord.Color.random(),
+        )
+        await interaction.response.send_message(
+            embed=em, view=are_you_sure, ephemeral=True
+        )
+        await are_you_sure.wait()
+
+        # if they hit no, do nothing
+        if not are_you_sure.accepted:
+            return
+
+        # send the closed ticket view
+        view = ClosedTicketView(self.ticket, self.member, self.client)
+        embed = discord.Embed(
+            title="Ticket has been closed!",
+            description=f"Closed by: {interaction.user.mention}\nTo view a transcript of messages in this \
+                channel hit the transcript button.\nNote that this will be an html file \
+                    so you'll need to download it and open in a webbrowser",
+        )
+        await interaction.followup.send(embed=embed, view=view)
+        await interaction.channel.set_permissions(
+            self.member, send_messages=False, read_messages=False
+        )
+        try:
+            await interaction.delete_original_message()
+        except discord.Forbidden:
+            return
+        self.stop()
+
+
+class ClosedTicketView(discord.ui.View):
+    def __init__(self, ticket_state: Ticket, member: discord.Member, client):
+        self.member = member
+        self.ticket = ticket_state
+        self.transcript = None
+        self.client = client
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Transcript", style=discord.ButtonStyle.grey, emoji="📄")
+    async def transcript_button(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.defer()
+        if self.transcript is None:
+            transcript = await chat_exporter.export(
+                interaction.channel,
+                limit=1000,
+                tz_info="UTC",
+                military_time=True,
+                bot=self.client,
+            )
+
+            if transcript is None:
+                return
+
+            self.transcript_file = io.BytesIO(transcript.encode())
+        await interaction.followup.send(
+            file=discord.File(
+                self.transcript_file,
+                filename=f"transcript-{interaction.channel.name}.html",
+            )
+        )
+
+    @discord.ui.button(label="Delete", style=discord.ButtonStyle.grey, emoji="🗑️")
+    async def delete_button(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        # send a button to confirm to close or not
+        are_you_sure = ConfirmView(target=interaction.user)
+        em = discord.Embed(
+            title="Close Ticket?",
+            description=(
+                f"{interaction.user.mention} are you sure you want to DELETE this ticket?\n\
+                    This action is permanent and can NOT be reversed"
+            ),
+            color=discord.Color.random(),
+        )
+        await interaction.response.send_message(
+            embed=em, view=are_you_sure, ephemeral=True
+        )
+        await are_you_sure.wait()
+        # if they hit no, do nothing
+        if not are_you_sure.accepted:
+            return
+
+        await interaction.channel.delete()
+        await self.client.db.execute(
+            "DELETE FROM tickets WHERE id=$1", self.ticket.ticket_id
+        )
+
+    @discord.ui.button(label="Reopen", style=discord.ButtonStyle.grey, emoji="🔓")
+    async def reopen_button(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.defer()
+
+        embed = discord.Embed(
+            title=f"Ticket from {self.member.name} has been reopened!",
+            description=f"Ticket was reopened by: {interaction.user}",
+        )
+        embed.set_footer(text="To close this ticket click the close button")
+        view = TicketView(self.ticket, self.member, self.client)
+        await interaction.followup.send(embed=embed, view=view)
+
+        perms = {
+            "send_messages": True,
+            "read_messages": True,
+            "add_reactions": True,
+            "embed_links": True,
+            "attach_files": True,
+            "read_message_history": True,
+            "external_emojis": True,
+        }
+        await interaction.channel.set_permissions(self.member, **perms)
+
+        try:
+            await interaction.delete_original_message()
+        except discord.Forbidden:
+            return
+        self.stop()
