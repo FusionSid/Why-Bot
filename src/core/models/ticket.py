5006f09a5e5bf4028b2e41acf27188088832f49c
FusionSid <siddheshadsv@icloud.com>
2022-12-21 00:24:58 +1300

    feat: Add new ticket button view

--- src/core/models/ticket.py
+++ src/core/models/ticket.py
@@ -1,8 +1,14 @@
+import time
 import io
-import chat_exporter
 from dataclasses import dataclass
-from core.helpers.views import ConfirmView
+
 import discord
+import chat_exporter
+
+from core.models.client import WhyBot
+from core.utils.formatters import discord_timestamp
+from core.db.setup_guild import setup_tickets
+from core.helpers.views import ConfirmView, InputModalView
 
 
 @dataclass
@@ -22,16 +28,6 @@ class Ticket:
     channel_id: int
     ticket_creator: int
     time_created: int
-
-
-async def get_button(guild_id: int) -> discord.ui.Button:
-    return discord.ui.Button()
-
-
-class NewTicketView(discord.ui.View):
-    def __init__(self, button: discord.ui.Button):
-        super().__init__(timeout=None)
-        self.add_item(button)
 
 
 class TicketView(discord.ui.View):
@@ -173,3 +169,114 @@ class ClosedTicketView(discord.ui.View):
         except discord.Forbidden:
             return
         self.stop()
+
+
+class NewTicketView(discord.ui.View):
+    def __init__(self, guild_id: int, client: WhyBot):
+        self.client = client
+        self.guild_id = guild_id
+        super().__init__(timeout=None)
+
+        button = discord.ui.Button(
+            label="New Ticket",
+            style=discord.ButtonStyle.gray,
+            emoji="📩",
+            custom_id=f"new-ticket-view-{guild_id}",
+        )
+        button.callback = self.callback
+        self.add_item(button)
+
+    async def callback(self, interaction: discord.Interaction):
+        # ik what ur thinking tHIs iS just COPy PaSTED codE fROm TIckEts.pY
+        # well stfu didnt ask im lazy ok
+        modal = InputModalView(
+            title="Ticket Reason", label="Enter the reason why you're making the ticket"
+        )
+        modal.children[0].max_length = 2000
+        await interaction.response.send_modal(modal)
+        await modal.wait()
+
+        if modal.value is None:
+            return await interaction.followup.send("Invalid Input", ephemeral=True)
+
+        data: list[list] = await self.client.db.fetch(
+            "SELECT * FROM ticket_guild WHERE guild_id=$1", self.guild_id
+        )
+        if len(data) == 0:
+            await setup_tickets(self.client.db, self.guild_id)
+            default_data = [self.guild_id, [], [], False, None, []]
+            ticket_config = TicketGuild(*default_data)
+        else:
+            ticket_config = TicketGuild(*data[0])
+
+        if interaction.user.id in ticket_config.banned_users:
+            return await interaction.followup.send(
+                "You are banned from making tickets (imagine)", ephemeral=True
+            )
+
+        # figure out if to make it in a category or not
+        if ticket_config.category in (0, None):
+            category = None
+        else:
+            category = list(
+                filter(
+                    lambda i: i.id == ticket_config.category,
+                    interaction.guild.categories,
+                )
+            )
+            category = None if len(category) == 0 else category[0]
+
+        # create channel
+        channel = await interaction.guild.create_text_channel(
+            f"ticket-{interaction.user.id}", category=category
+        )
+
+        # set channel perms
+        await channel.set_permissions(
+            interaction.guild.get_role(interaction.guild.id),
+            send_messages=False,
+            read_messages=False,
+        )
+        perms = {
+            "send_messages": True,
+            "read_messages": True,
+            "add_reactions": True,
+            "embed_links": True,
+            "attach_files": True,
+            "read_message_history": True,
+            "external_emojis": True,
+        }
+        await channel.set_permissions(interaction.user, **perms)
+        for role in ticket_config.roles_allowed:
+            if role := interaction.guild.get_role(role) is not None:
+                await channel.set_permissions(role, **perms)
+
+        ticket_data = [
+            interaction.guild.id,
+            channel.id,
+            interaction.user.id,
+            int(time.time()),
+        ]
+        ticket_id = await self.client.db.fetch(
+            """INSERT INTO tickets (
+                guild_id, channel_id, ticket_creator, time_created
+            ) VALUES ($1, $2, $3, $4) RETURNING id""",
+            *ticket_data,
+        )
+
+        ticket = Ticket(ticket_id[0][0], *ticket_data)
+
+        embed = discord.Embed(
+            title=f"New ticket from {interaction.user.name}!",
+            description=f"**Please wait, support will be with you shortly!**\
+                \n\nTicket Created: {await discord_timestamp(ticket.time_created, 'ts')}",
+            color=discord.Color.random,
+        )
+        embed.add_field(name="Reason Provided:", value=str(modal.value))
+        embed.set_footer(text="To close this ticket click the close button")
+        view = TicketView(ticket, interaction.user, self.client)
+        await channel.send(embed=embed, view=view)
+
+        await interaction.followup.send(
+            f"A new ticket has been created for you: {channel.mention}", ephemeral=True
+        )
