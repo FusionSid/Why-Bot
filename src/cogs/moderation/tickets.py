698200eea3e9216aa6c74543bbde61e6c3b89147
FusionSid <siddheshadsv@icloud.com>
2022-12-23 22:52:49 +1300

    feat(ticket): Added a slash command for all ticket functions

--- src/cogs/moderation/tickets.py
+++ src/cogs/moderation/tickets.py
@@ -1,6 +1,8 @@
+import io
 import time
 
 import discord
+import chat_exporter
 from discord.ext import commands
 from discord.commands import SlashCommandGroup, default_permissions
 
@@ -9,7 +11,13 @@ from core.helpers.views import ConfirmView
 from core.db.setup_guild import setup_tickets
 from core.utils.formatters import discord_timestamp
 from core.helpers.checks import run_bot_checks
-from core.models.ticket import TicketGuild, Ticket, TicketView, NewTicketView
+from core.models.ticket import (
+    TicketGuild,
+    Ticket,
+    TicketView,
+    NewTicketView,
+    ClosedTicketView,
+)
 
 
 class Tickets(commands.Cog):
@@ -95,52 +103,105 @@ class Tickets(commands.Cog):
             color=discord.Color.random(),
         )
         embed.add_field(name="Reason Provided:", value=str(reason))
+        embed.add_field("Ticket ID:", value=ticket.ticket_id)
         embed.set_footer(text="To close this ticket click the close button")
         view = TicketView(ticket, ctx.author, self.client)
-        await channel.send(embed=embed, view=view)
+
+        who_to_ping = filter(
+            lambda x: x is not None, map(ctx.guild.get_role, ticket_config.ping_roles)
+        )
+        await channel.send(
+            embed=embed, view=view, content="".join(i.mention for i in who_to_ping)
+        )
 
         await ctx.respond(f"A new ticket has been created for you: {channel.mention}")
 
-    # TODO
-    # @ticket.command()
-    # async def close(self, ctx: discord.ApplicationContext):
-    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+    @ticket.command()
+    async def close(self, ctx: discord.ApplicationContext):
+        await ctx.defer()
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def rename(self, ctx: discord.ApplicationContext):
-    #     tickets = await self.__get_tickets(ctx.guild.id)
+        tickets = await self.__get_tickets(ctx.guild.id)
+        ticket = list(filter(lambda t: t.channel_id == ctx.channel_id, tickets))
+        if not ticket:
+            embed = discord.Embed(
+                title="Could Not Find Ticket",
+                description="This channel is not a ticket",
+                color=discord.Color.random(),
+            )
+            return await ctx.respond(embed=embed, ephemeral=True)
+        ticket: Ticket = ticket[0]
+        are_you_sure = ConfirmView(target=ctx.author)
+        em = discord.Embed(
+            title="Close Ticket?",
+            description=(
+                f"{ctx.author.mention} are you sure you want to close this ticket?\nThis action removes \
+                    access for the person who made the ticket from the channel\nHowever this action can be reversed"
+            ),
+            color=discord.Color.random(),
+        )
+        await ctx.respond(embed=em, view=are_you_sure, ephemeral=True)
+        await are_you_sure.wait()
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def add_ping_role(self, ctx: discord.ApplicationContext):
-    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        # if they hit no, do nothing
+        if not are_you_sure.accepted:
+            return
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def remove_ping_role(self, ctx: discord.ApplicationContext):
-    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        # send the closed ticket view
+        member = ctx.guild.get_member(ticket.ticket_creator)
+        if member is None:
+            return
+        view = ClosedTicketView(ticket, ctx.author, self.client)
+        embed = discord.Embed(
+            title="Ticket has been closed!",
+            description=f"Closed by: {ctx.author.mention}\nTo view a transcript of messages in this \
+                channel hit the transcript button.\nNote that this will be an html file \
+                    so you'll need to download it and open in a webbrowser",
+        )
+        await ctx.send(embed=embed, view=view)
+        await ctx.channel.set_permissions(
+            member, send_messages=False, read_messages=False
+        )
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def add_allowed_role(self, ctx: discord.ApplicationContext):
-    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def rename(self, ctx: discord.ApplicationContext, new_name: str):
+        await ctx.defer()
+        tickets = await self.__get_tickets(ctx.guild.id)
+        ticket = list(filter(lambda t: t.channel_id == ctx.channel_id, tickets))
+        if not ticket:
+            embed = discord.Embed(
+                title="Could Not Find Ticket",
+                description="This channel is not a ticket",
+                color=discord.Color.random(),
+            )
+            return await ctx.respond(embed=embed, ephemeral=True)
+        ticket = ticket[0]
+        await ctx.channel.edit(name=new_name)
+        await ctx.respond("Ticket name updated!")
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def remove_allowed_role(self, ctx: discord.ApplicationContext):
-    #     ticket_config = await self.__get_ticket_config(ctx.guild.id)
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def transcript(self, ctx: discord.ApplicationContext):
+        transcript = await chat_exporter.export(
+            ctx.channel,
+            limit=1000,
+            tz_info="UTC",
+            military_time=True,
+            bot=self.client,
+        )
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def transcript(self, ctx: discord.ApplicationContext):
-    #     tickets = await self.__get_tickets(ctx.guild.id)
+        if transcript is None:
+            return await ctx.respond("Failed to generate transcript", ephemeral=True)
+
+        self.transcript_file = io.BytesIO(transcript.encode())
+        await ctx.respond(
+            file=discord.File(
+                self.transcript_file,
+                filename=f"transcript-{ctx.channel.name}.html",
+            )
+        )
 
     @ticket.command()
     @default_permissions(administrator=True)
@@ -160,11 +221,28 @@ class Tickets(commands.Cog):
             "UPDATE ticket_guild SET create_button=true WHERE guild_id=$1", ctx.guild.id
         )
 
-    # @ticket.command()
-    # @default_permissions(administrator=True)
-    # @commands.has_permissions(administrator=True)
-    # async def delete(self, ctx: discord.ApplicationContext):
-    #     tickets = await self.__get_tickets(ctx.guild.id)
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def delete(self, ctx: discord.ApplicationContext):
+        await ctx.defer()
+        tickets = await self.__get_tickets(ctx.guild.id)
+        ticket = list(filter(lambda t: t.channel_id == ctx.channel_id, tickets))
+        if not ticket:
+
+            embed = discord.Embed(
+                title="Could Not Find Ticket",
+                description="This channel is not a ticket",
+                color=discord.Color.random(),
+            )
+            return await ctx.respond(embed=embed, ephemeral=True)
+        ticket: Ticket = ticket[0]
+        await self.client.db.execute(
+            "DELETE FROM tickets WHERE id=$1 AND guild_id=$2",
+            ticket.ticket_id,
+            ctx.guild.id,
+        )
+        await ctx.channel.delete()
 
     @ticket.command()
     @default_permissions(administrator=True)
@@ -196,6 +274,141 @@ class Tickets(commands.Cog):
             "DELETE FROM tickets WHERE guild_id=$1", ctx.guild.id
         )
         await ctx.send("All tickets have been deleted!")
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def add_ping_role(self, ctx: discord.ApplicationContext, role: discord.Role):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if role.id in ticket_config.ping_roles:
+            return await ctx.respond(
+                "Role is already in list for being pinged when tickets are made"
+            )
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET ping_roles = array_append(ping_roles, $1), \
+                roles_allowed = array_append(roles_allowed, $1) WHERE guild_id=$2",
+            role.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="Ping Role Added",
+                description=f"{role.mention} will now be pinged when a new ticket is made",
+                color=discord.Color.random(),
+            )
+        )
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def remove_ping_role(
+        self, ctx: discord.ApplicationContext, role: discord.Role
+    ):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if role.id not in ticket_config.ping_roles:
+            return await ctx.respond(
+                "Role was already not in the list for being pinged when tickets are made"
+            )
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET ping_roles = array_remove(ping_roles, $1) WHERE guild_id=$2",
+            role.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="Ping Role Removed",
+                description=f"{role.mention} will no longer be pinged when a new ticket is made",
+                color=discord.Color.random(),
+            )
+        )
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def add_allowed_role(
+        self, ctx: discord.ApplicationContext, role: discord.Role
+    ):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if role.id in ticket_config.roles_allowed:
+            return await ctx.respond(
+                "Role is already in the list that have access to new tickets"
+            )
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET roles_allowed = array_append(roles_allowed, $1) WHERE guild_id=$2",
+            role.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="Role Access Added",
+                description=f"{role.mention} now has access to new tickets",
+                color=discord.Color.random(),
+            )
+        )
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def remove_allowed_role(
+        self, ctx: discord.ApplicationContext, role: discord.Role
+    ):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if role.id not in ticket_config.roles_allowed:
+            return await ctx.respond(
+                "Role is already not in the list that have access to new tickets"
+            )
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET roles_allowed = array_remove(roles_allowed, $1) WHERE guild_id=$2",
+            role.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="Role Access Removed",
+                description=f"{role.mention} no longer has access to new tickets",
+                color=discord.Color.random(),
+            )
+        )
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def ban_user(self, ctx: discord.ApplicationContext, member: discord.Member):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if member.id in ticket_config.banned_users:
+            return await ctx.respond("User is already banned from making tickets")
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET banned_users = array_append(banned_users, $1) WHERE guild_id=$2",
+            member.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="User Banned",
+                description=f"{member.mention} is banned from making tickets",
+                color=discord.Color.random(),
+            )
+        )
+
+    @ticket.command()
+    @default_permissions(administrator=True)
+    @commands.has_permissions(administrator=True)
+    async def unban_user(self, ctx: discord.ApplicationContext, member: discord.Member):
+        ticket_config = await self.__get_ticket_config(ctx.guild.id)
+        if member.id not in ticket_config.banned_users:
+            return await ctx.respond("User is already unbanned from making tickets")
+        await self.client.db.execute(
+            "UPDATE ticket_guild SET banned_users = array_remove(banned_users, $1) WHERE guild_id=$2",
+            member.id,
+            ctx.guild.id,
+        )
+        await ctx.respond(
+            embed=discord.Embed(
+                title="User Unbanned",
+                description=f"{member.mention} is no longer banned from making tickets",
+                color=discord.Color.random(),
+            )
+        )
 
 
 def setup(client):
